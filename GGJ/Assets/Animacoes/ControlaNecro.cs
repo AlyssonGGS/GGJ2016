ac01659b54236da29fd235d39ea0fbff3b1a61b6
AlyssonGGS <alyssonggs@hotmail.com>
2016-01-30 21:52:04 -0200

    Animações
    Adicionada animação do necro com teclas funcionando

---
+++ GGJ/Assets/Animacoes/ControlaNecro.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlaNecro : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Animator>().Play("Necro_Up");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Animator>().Play("Necro_Left");
+        }
+
+    }
+}
